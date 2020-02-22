85573c9699d1c9afbe75ee2040fd9bcbb80192e6
lagerhxx <2548457162@QQ.com>
2020-02-22 15:26:21 +0800

    测试

---
+++ src/test/java/com/hxx/story/StoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxx.story;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
